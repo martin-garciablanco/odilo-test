7547d62ca8d59776d09d6405bc966047ca6dfc42
martin-garciablanco <programadopormartin@gmail.com>
2023-09-27 13:49:47 +0200

    feat(server): :sparkles: Cache Clubs when retrieving from db

--- server/src/clubs/clubController.js
+++ server/src/clubs/clubController.js
@@ -1,10 +1,20 @@
 'use strict';
 
 var mongoose = require('mongoose');
+const cache = require('../../cache');
 var Club = mongoose.model('Club');
 
-module.exports.list = function(req, res) {
-    Club.find()
-        .then(c => res.jsonp(c))
-        .catch(error => res.status(500).send({message: error}));;
+const CLUBS_CACHE_KEY = "clubs";
+const CLUBS_CACHE_TTL = 300;
+
+module.exports.list = function (req, res) {
+    const clubsFromCache = cache.get(CLUBS_CACHE_KEY);
+    clubsFromCache
+        ? res.jsonp(clubsFromCache)
+        : Club.find()
+            .then(clubs => {
+                cache.set(CLUBS_CACHE_KEY, clubs, CLUBS_CACHE_TTL)
+                res.jsonp(clubs)
+            })
+            .catch(error => res.status(500).send({ message: error }));
 }
